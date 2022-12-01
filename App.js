a0e510bf992329249dee959795b996ec02da5157
Gdempc <grzegorz.dempc@gmail.com>
2022-12-01 16:24:20 +0100

    a

--- App.js
+++ App.js
@@ -22,43 +22,13 @@ const Cat = (props) => {
 
 const Cafe = () => {
   return (
-<<<<<<< HEAD
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            x:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-=======
+
+
     <>
       <Cat name="Munkustrap" />
       <Cat name="Spot" />
     </>
->>>>>>> origin/master
+
   );
 }
 
