c2d90e1cd6e2519fbaec77c3d39fa9e4b34b4911
matvii <vladtratata10@gmail.com>
2022-12-08 17:21:42 +0100

    first step

--- App.js
+++ App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
-
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, Dimensions } from 'react-native';
 const App = () => (
-  
     <View style={styles.container1}>
-    <Button
+    <View style={styles.button} 
+      Button
       title="1"
       onPress={() => Alert.alert('Simple Button pressed')} />
-      <Button
-        title="2"
-        onPress={() => Alert.alert('Simple Button pressed')} />
-      <Button
-        title="3"
-        onPress={() => Alert.alert('Simple Button pressed')} />
-  </View>
+    </View>
 );
 
 const styles = StyleSheet.create({
   container1: {
     flex: 1,
-    margin:10,
-    justifyContent:'center',
-    height:50,
-    borderColor:'gray',
-    borderWidth:2
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderColor: 'gray',
+    borderWidth: 2,
+    top: 1,
+  },
+  button: {
+    flex: 1,
+    width: 100,
   }
 });
 
