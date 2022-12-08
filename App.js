7cfe875c7f2b8dd143a34fca5cc3f6b8f4ebe339
Gdempc <grzegorz.dempc@gmail.com>
2022-12-08 16:35:10 +0100

    calc base

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-const Calc = (props) => {
+const App = () => (
   
-}
+    <View style={styles.container1}>
+    <Button
+      title="1"
+      onPress={() => Alert.alert('Simple Button pressed')} />
+      <Button
+        title="2"
+        onPress={() => Alert.alert('Simple Button pressed')} />
+      <Button
+        title="3"
+        onPress={() => Alert.alert('Simple Button pressed')} />
+  </View>
+);
 
-export default Calc;
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    margin:10,
+    justifyContent:'center',
+    height:50,
+    borderColor:'gray',
+    borderWidth:2
+  }
+});
+
+export default App;
